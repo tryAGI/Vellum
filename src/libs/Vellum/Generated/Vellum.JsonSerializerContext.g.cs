58edd84e48e945d09573691b21ba4c4f5ee2efe0
Konstantin S. <havendv@gmail.com>
2026-08-29 13:56:02 +0400

    feat: Updated OpenAPI spec (#46)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Vellum/Generated/Vellum.JsonSerializerContext.g.cs
+++ src/libs/Vellum/Generated/Vellum.JsonSerializerContext.g.cs
@@ -16,7 +16,7 @@ namespace Vellum
         })]
     [global::System.Text.Json.Serialization.JsonSerializable(typeof(global::System.Collections.Generic.Dictionary<string, string>))]
     [global::System.Text.Json.Serialization.JsonSerializable(typeof(global::System.Collections.Generic.Dictionary<string, object>))]
-    [global::System.Text.Json.Serialization.JsonSerializable(typeof(global::System.Collections.Generic.List<object>))]
+    [global::System.Text.Json.Serialization.JsonSerializable(typeof(global::System.Collections.Generic.List<object>), TypeInfoPropertyName = "SystemCollectionsGeneric_ObjectList")]
     [global::System.Text.Json.Serialization.JsonSerializable(typeof(global::System.Text.Json.JsonElement?))]
     [global::System.Text.Json.Serialization.JsonSerializable(typeof(global::Vellum.StringEnum), TypeInfoPropertyName = "StringEnum2")]
     [global::System.Text.Json.Serialization.JsonSerializable(typeof(global::Vellum.StringInputRequest))]
